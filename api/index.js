3cd944e79ed1c319b6f4b2727a98e4229cacca60
waibaBikash <kavrelibikash@gmail.com>
2024-07-30 23:07:32 +1000

    commit change

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ dotenv.config();
 mongoose
 .connect(process.env.MONGO)
 .then(()=>{
+  
   console.log('Connected to MongoDB!');
 })
   .catch((err)=>{
@@ -15,6 +16,6 @@ mongoose
 const app = express();
 
 app.listen(3000, () => {
-  
+
   console.log('Server is running on port 3000!');
 });
