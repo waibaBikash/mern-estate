6baa60beb88cefec1728ae6d4f7bccada1a44957
waibaBikash <kavrelibikash@gmail.com>
2024-07-30 23:04:08 +1000

    save change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,8 +25,6 @@ export default function Header() {
             <Link to="/sign-in">
             <li className='text-slate-700 hover:underline'>Sign in</li>
             </Link>
-
-             
            </ul>
          </div>
     </header>
