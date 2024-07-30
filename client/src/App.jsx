b727ca299133b098e39fe9d6266864a1d6496746
waibaBikash <kavrelibikash@gmail.com>
2024-07-30 23:11:03 +1000

    commit change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes , Route } from "react-router-dom"
+import { BrowserRouter, Routes , Route } from "react-router-dom";
 import Home from "./pages/Home"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
