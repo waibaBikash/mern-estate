771cda2cd970f775e35caa102a4e2778b5d337e4
waibaBikash <kavrelibikash@gmail.com>
2024-07-31 19:22:14 +1000

    Create a test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 
 dotenv.config(); // initialize
 
@@ -23,3 +25,4 @@ app.listen(3000, () => {
 
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
