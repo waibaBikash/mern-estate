8d15312c05f44eb83439d5fa19e7205df44432e6
waibaBikash <kavrelibikash@gmail.com>
2024-08-01 21:17:02 +1000

    commit change

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -45,8 +45,20 @@ export default function SignUp() {
     <div className='p-3 max-w-lg mx-auto'>
        <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
         <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-           <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
-           <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+           <input 
+           type="text" placeholder='username' 
+           className='border p-3 rounded-lg'
+           id='username' 
+           onChange={handleChange}
+            />
+
+           <input 
+           type="email" placeholder='email' 
+           className='border p-3 rounded-lg' 
+           id='email' 
+           onChange={handleChange}
+            />
+            
            <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
            <button 
            disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-85'>
