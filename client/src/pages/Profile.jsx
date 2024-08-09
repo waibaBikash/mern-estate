04dd37fce089f19a16c55e7f76ae73895c4935fe
waibaBikash <kavrelibikash@gmail.com>
2024-08-09 21:44:16 +1000

    create a profile page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import {useSelector} from 'react-redux';
 
 export default function Profile() {
+   const {currentUser} = useSelector((state) => state.user)
   return (
       <div>
         <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+        <form>
+           <img src={currentUser.avatar} alt="profile" className='rounded-full' />
+        </form>
       </div>
   )
 }
