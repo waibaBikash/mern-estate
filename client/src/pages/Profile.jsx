1397176d73c87d524ec610decf533d7e7e45376a
waibaBikash <kavrelibikash@gmail.com>
2024-08-09 20:42:33 +1000

    commit change

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+      <div>
+        <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      </div>
   )
 }
