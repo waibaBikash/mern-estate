4bfec8dfd94f173069f30e631ff9ad368b73f6ed
waibaBikash <kavrelibikash@gmail.com>
2024-08-12 18:08:59 +1000

    save change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,8 @@ export default function Header() {
             </Link>
             <Link to="/profile">
             {currentUser ? (
-               <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile" />
+               <img className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar} alt="profile" />
             ): <li className='text-slate-700 hover:underline'>Sign in</li>
             }
             </Link>
