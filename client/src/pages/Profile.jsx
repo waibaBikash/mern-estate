32ebac71b2e325cb01f20778522ba1d54d691bd9
waibaBikash <kavrelibikash@gmail.com>
2024-08-14 21:10:08 +1000

    save change

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,9 @@
 import {useSelector} from 'react-redux';
 import {useEffect, useRef, useState} from 'react';
-import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import { getDownloadURL, 
+   getStorage, 
+   ref, 
+   uploadBytesResumable } from 'firebase/storage';
 import {app} from '../firebase';
 
 
