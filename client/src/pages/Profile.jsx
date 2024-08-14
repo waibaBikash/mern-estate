a6164646703d6ad175b604d5ae8b2ba06f646ef5
waibaBikash <kavrelibikash@gmail.com>
2024-08-14 21:23:32 +1000

    commit change

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -87,7 +87,6 @@ export default function Profile() {
                   ) : (
                      ""
                   )}
-               
            </p>
            <input 
            type="text" 
