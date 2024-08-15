10e62a98482b92bc45882e5686e18a6068ce3bc0
waibaBikash <kavrelibikash@gmail.com>
2024-08-15 22:37:58 +1000

    commit chnage

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -73,7 +73,8 @@ export default function Profile() {
            onClick={()=>fileRef.current.click()}
             src={formData.avatar || currentUser.avatar} 
             alt="profile" 
-            className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' 
+            className='rounded-full h-24 w-24 object-cover
+             cursor-pointer self-center mt-2' 
             />
            <p className='text-sm self-center'>
              {fileUploadError ? (
