6921e83bec2c8832ad70dd2d000cd20fb84ba506
waibaBikash <kavrelibikash@gmail.com>
2024-08-15 22:09:40 +1000

    save change

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -35,7 +35,7 @@ export default function Profile() {
      const handleFileUpload = (file) => {
      const storage = getStorage(app);
      const fileName = new Date().getTime() + file.name;
-     const storageRef = ref(storage,fileName);
+     const storageRef = ref(storage, fileName);
      const uploadTask = uploadBytesResumable(storageRef, file);
 
      uploadTask.on(
