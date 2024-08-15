de5ea0a56faf9a332c1f5c80728668fe5fe83c85
waibaBikash <kavrelibikash@gmail.com>
2024-08-15 22:25:03 +1000

    save change

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -89,7 +89,7 @@ export default function Profile() {
                       Image succesfully uploaded!
                      </span>
                   ) : (
-                     ""
+                    ''
                   )}
            </p>
            <input 
