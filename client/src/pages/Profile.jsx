d2353ee7a08ac042d153e5ef1b46224b6254f9e7
waibaBikash <kavrelibikash@gmail.com>
2024-08-16 21:31:16 +1000

    save change

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -57,7 +57,7 @@ export default function Profile() {
   };
 
   return (
-      <div className='p-3 max-w-lg mx-auto '>
+      <div className='p-3 max-w-lg mx-auto'>
         <h1 
         className='text-3xl font-semibold text-center my-7'>Profile
         </h1>
