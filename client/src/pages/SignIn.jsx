a8e9e987c51b86de8e3831f6d0489c0c3317a5f5
waibaBikash <kavrelibikash@gmail.com>
2024-08-17 21:52:04 +1000

    commit change

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -65,7 +65,9 @@ export default function SignIn() {
             <OAuth/>
         </form>
          <div className='flex gap-2 mt-5'>
-           <p>Dont have an account?</p>
+            <p>
+              Dont have an account?
+            </p>
            <Link to="/sign-up">
                <span 
                 className='text-blue-700'>Sign up
