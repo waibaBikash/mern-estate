f9614945243823c6f40beae4eb2facf46317c1cd
waibaBikash <kavrelibikash@gmail.com>
2024-08-17 21:42:46 +1000

    commit change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,7 +25,9 @@ export default function Header() {
              </li>
             </Link>
             <Link to="/about"> 
-            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+            <li 
+            className='hidden sm:inline text-slate-700 hover:underline'>About
+            </li>
             </Link>
             <Link to="/profile">
             {currentUser ? (
