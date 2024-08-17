0ef7719b1968107bc8dd6ccc193c5698cb4963c6
waibaBikash <kavrelibikash@gmail.com>
2024-08-17 21:52:57 +1000

    commit change

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -43,7 +43,9 @@ export default function SignIn() {
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-       <h1 className='text-3xl text-center font-semibold my-7'>Sign in</h1>
+        <h1 
+         className='text-3xl text-center font-semibold my-7'>Sign in
+        </h1>
         <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
            <input 
            type="email" placeholder='email' 
