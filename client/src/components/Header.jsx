72e426b90c22e328c97786f2d2e1f55ad602679c
waibaBikash <kavrelibikash@gmail.com>
2024-08-17 21:46:06 +1000

    commit change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,8 +15,10 @@ export default function Header() {
        </Link>
          <form 
            className='bg-slate-100 p-3 rounded-lg flex items-center'>
-           <input type="text" placeholder='Search...' 
-           className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+           <input
+            type="text" placeholder='Search...' 
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+           />
            <FaSearch className='text-slate-600'/>
          </form>
            <ul className='flex gap-4'>
