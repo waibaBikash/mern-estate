84ec96541178098a69462e1cebbfebce8f25b0eb
waibaBikash <kavrelibikash@gmail.com>
2024-08-17 21:47:00 +1000

    save change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,7 +9,9 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to="/">
        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-         <span className='text-slate-500'>Bikash</span>
+         <span 
+         className='text-slate-500'>Bikash
+         </span>
          <span className='text-slate-700'>Estate</span>
        </h1>
        </Link>
