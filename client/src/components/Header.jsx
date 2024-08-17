e67825aaa1761fb2d3ab908474ce39e596ca3f2f
waibaBikash <kavrelibikash@gmail.com>
2024-08-17 21:45:05 +1000

    save change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,25 +20,25 @@ export default function Header() {
            <FaSearch className='text-slate-600'/>
          </form>
            <ul className='flex gap-4'>
-            <Link to="/">
-            <li
-             className='hidden sm:inline text-slate-700 hover:underline'>Home
+              <Link to="/">
+              <li
+              className='hidden sm:inline text-slate-700 hover:underline'>Home
+              </li>
+              </Link>
+              <Link to="/about"> 
+             <li 
+             className='hidden sm:inline text-slate-700 hover:underline'>About
              </li>
-            </Link>
-            <Link to="/about"> 
-            <li 
-            className='hidden sm:inline text-slate-700 hover:underline'>About
-            </li>
-            </Link>
-            <Link to="/profile">
-            {currentUser ? (
+             </Link>
+             <Link to="/profile">
+             {currentUser ? (
                <img className='rounded-full h-7 w-7 object-cover'
                 src={currentUser.avatar} alt="profile" />
-            ): <li 
-            className='text-slate-700 hover:underline'>Sign in
-            </li>
-            }
-            </Link>
+             ): <li 
+             className='text-slate-700 hover:underline'>Sign in
+             </li>
+             }
+             </Link>
 
            </ul>
          </div>
