59f61eb9e8338e12f87978c02abbd33eb0b33e71
waibaBikash <kavrelibikash@gmail.com>
2024-08-17 21:43:43 +1000

    commit change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,7 +13,8 @@ export default function Header() {
          <span className='text-slate-700'>Estate</span>
        </h1>
        </Link>
-         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+         <form 
+           className='bg-slate-100 p-3 rounded-lg flex items-center'>
            <input type="text" placeholder='Search...' 
            className='bg-transparent focus:outline-none w-24 sm:w-64'/>
            <FaSearch className='text-slate-600'/>
